d3fa46ab2095adb9e2fa2e5b2bca93388a0776a9
24073zj <24073zj@hvhs.school.nz>
2026-04-27 21:44:58 +0000

    Apr 27, 2026, 9:44 PM

--- t02_variables.js
+++ t02_variables.js
@@ -2,8 +2,20 @@
 This is a block comment.
 task 2 variables
 ****************************/
-var score = 10;
+var name = "Zac";
 
-console.log("Running t01_introduction.js");
+var age = 15;
+
+var pocketMoney = 10;
+
+var year = 2026;
+
+console.log("Running t02_variables.js");
+console.log("Hi " + name);
 console.log(`Score is ${score}.`);
+
+
+As of ____ you are __ years old
+
+You have __ dollars
 
