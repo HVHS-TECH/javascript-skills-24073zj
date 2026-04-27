8458f406bd119fac7861a042c61dfa28280c6ecc
24073zj <24073zj@hvhs.school.nz>
2026-04-27 21:59:54 +0000

    Apr 27, 2026, 9:59 PM

--- t03_maths.js
+++ t03_maths.js
@@ -3,7 +3,7 @@ task 3 maths
 ****************************/
 var username = "Zac";
 
-var age = 15;
+var age = 16;
 
 var pocketMoney = 20;
 
@@ -16,4 +16,4 @@ console.log(`You were born in ${year - age}`);
 console.log(`In 10 years you will be ${age + 10} years old`);
 console.log(`You have ${pocketMoney} dollars`);
 console.log(`You spend half of your money, now you have ${pocketMoney / 2} dollars`);
-console.log(`Then you get $3, now you have ${}`);
+console.log(`Then you get $3, now you have ${(pocketMoney / 2) + 3}`);
