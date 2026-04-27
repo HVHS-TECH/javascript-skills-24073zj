fb3251c9f8fd6b8557aa00803774555212d90d66
24073zj <24073zj@hvhs.school.nz>
2026-04-27 21:24:03 +0000

    Apr 27, 2026, 9:24 PM

--- t02_variables.js
+++ t02_variables.js
@@ -1,1 +1,9 @@
-/* Paste your code from task 1 here */
+/****************************
+This is a block comment.
+task 2 variables
+****************************/
+var score = 10;
+
+console.log("Running t01_introduction.js");
+console.log(`Score is ${score}.`);
+
