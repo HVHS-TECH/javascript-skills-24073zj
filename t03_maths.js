1733048d8a0447f81d45c19c91584fa832e4546d
24073zj <24073zj@hvhs.school.nz>
2026-04-27 21:57:26 +0000

    Apr 27, 2026, 9:57 PM

--- t03_maths.js
+++ t03_maths.js
@@ -1,1 +1,19 @@
-/* Paste your code from task 2 here */
+/****************************
+task 3 maths
+****************************/
+var username = "Zac";
+
+var age = 15;
+
+var pocketMoney = 20;
+
+var year = 2026;
+
+console.log("Running t02_variables.js");
+console.log(`Hi ${username}`);
+console.log(`As of ${year} you are ${age} years old`);
+console.log(`You were born in ${year - age}`);
+console.log(`In 10 years you will be ${age + 10} years old`);
+console.log(`You have ${pocketMoney} dollars`);
+console.log(`You spend half of your money, now you have ${pocketMoney / 2} dollars`);
+console.log(`Then you get $3, now you have ${}`);
