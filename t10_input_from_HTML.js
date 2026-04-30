39a2537e6a7477876fe6a404634ef71a435bcbbb
24073zj <24073zj@hvhs.school.nz>
2026-04-30 01:26:28 +0000

    Apr 30, 2026, 1:26 AM

--- t10_input_from_HTML.js
+++ t10_input_from_HTML.js
@@ -3,12 +3,12 @@ task 10 Input from HTML
 ****************************/
 
 const INPUT_FIELD = document.getElementById("nameField");
-let username = INPUT_FIELD.value;
+var username = INPUT_FIELD.value;
 
 var OUTPUT = document.getElementById("spaceForJavaScriptOutput");
 
 function getFormInput(){
     console.log("Running t10_input_from_HTML.js");
 
-    OUTPUT.innerHTML = "<p>Welcome to the page " + "a" + "</p>";
+    OUTPUT.innerHTML.value = `<p>Welcome to the page ${username}</p>`;
 }
