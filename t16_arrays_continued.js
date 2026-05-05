c18812e54a4442951d13abc2fa4e311af6844887
24073zj <24073zj@hvhs.school.nz>
2026-05-05 23:43:13 +0000

    May 5, 2026, 11:43 PM

--- t16_arrays_continued.js
+++ t16_arrays_continued.js
@@ -1,1 +1,30 @@
-/* Paste your code from the last task */
+/****************************
+task 16 Arrays Continued
+****************************/
+
+const AMOUNTFIELD = document.getElementById("amountField");
+
+const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
+
+var amount = 0;
+
+function getFormInput(){
+    console.log("Running t16_arrays_continued.js");
+
+    amount = AMOUNTFIELD.value;
+
+    OUTPUT.innerHTML = "Writing song starting at: " + amount + ".<br>";
+
+    for(let i = amount; i > 0; i--)
+    {
+        let n = i;
+        if(n == 1){
+            OUTPUT.innerHTML += "1 bottle of milk on the wall, "+"1 bottle of milk. ";
+            OUTPUT.innerHTML += "Take it down, pass it around, "+"no more bottles of milk on the wall.<br><br>";
+        }else{
+            OUTPUT.innerHTML += n + " bottles of milk on the wall, " + n + " bottles of milk. ";
+            OUTPUT.innerHTML += "Take one down, pass it around, " + (n - 1) + " bottles of milk on the wall.<br>";
+        }
+    }
+    
+}
