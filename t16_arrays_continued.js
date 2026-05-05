dce6be4576b010093500db608da7533bafae6f22
24073zj <24073zj@hvhs.school.nz>
2026-05-05 23:54:39 +0000

    May 5, 2026, 11:54 PM

--- t16_arrays_continued.js
+++ t16_arrays_continued.js
@@ -8,23 +8,17 @@ const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
 
 var shoppingList = [];
 
+var firstTime = true;
+
 function getFormInput(){
-    console.log("Running t16_arrays_continued.js");
-
-    shoppingList.push = AMOUNTFIELD.value;
-}
-function ShowList(){
-    OUTPUT.innerHTML = "Writing song starting at: " + amount + ".<br>";
-
-    for(let i = amount; i > 0; i--)
-    {
-        let n = i;
-        if(n == 1){
-            OUTPUT.innerHTML += "1 bottle of milk on the wall, "+"1 bottle of milk. ";
-            OUTPUT.innerHTML += "Take it down, pass it around, "+"no more bottles of milk on the wall.<br><br>";
-        }else{
-            OUTPUT.innerHTML += n + " bottles of milk on the wall, " + n + " bottles of milk. ";
-            OUTPUT.innerHTML += "Take one down, pass it around, " + (n - 1) + " bottles of milk on the wall.<br>";
-        }
+    if(firstTime){
+        console.log("Running t16_arrays_continued.js");
+        firstTime = false;
     }
+    if(ITEMFIELD.value != undefined){
+        shoppingList.push(ITEMFIELD.value);
+    }
+}
+function showList(){
+    OUTPUT.innerHTML = shoppingList;
 }
