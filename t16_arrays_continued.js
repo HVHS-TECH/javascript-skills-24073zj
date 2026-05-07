9c57f4e27304b07e8cc263fe73dfa23b81eed77d
24073zj <24073zj@hvhs.school.nz>
2026-05-07 00:48:24 +0000

    May 7, 2026, 12:48 AM

--- t16_arrays_continued.js
+++ t16_arrays_continued.js
@@ -15,10 +15,22 @@ function getFormInput(){
         console.log("Running t16_arrays_continued.js");
         firstTime = false;
     }
-    if(ITEMFIELD.value != undefined){
+    if(ITEMFIELD.value != ""){
         shoppingList.push(ITEMFIELD.value);
+        ITEMFIELD.value = "";
     }
+
+    OUTPUT.innerHTML = "You have added " + shoppingList[shoppingList.length - 1] +" to the list";
 }
 function showList(){
-    OUTPUT.innerHTML = shoppingList;
+    OUTPUT.innerHTML = "You have added " + shoppingList[shoppingList.length - 1] +" to the list";
+    OUTPUT.innerHTML += "<ul>";
+    OUTPUT.innerHTML += "These are the items on your shopping list:";
+
+    for (let i = 0; i < shoppingList.length; i++) {
+        OUTPUT.innerHTML += `<li style="margin-left: 20px;">` + shoppingList[i] + "</li>";
+    }
+
+    OUTPUT.innerHTML += "</ul>";
+    
 }
