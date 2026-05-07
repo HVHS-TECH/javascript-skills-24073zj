711f96f24a69ba208e0d359075399d4e5dccdc70
24073zj <24073zj@hvhs.school.nz>
2026-05-07 00:57:07 +0000

    May 7, 2026, 12:57 AM

--- t18_validation.js
+++ t18_validation.js
@@ -1,1 +1,35 @@
-/* Paste your code from the last task */
+/****************************
+task 18 Validation
+****************************/
+
+const ITEMFIELD = document.getElementById("itemField");
+
+const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
+
+var shoppingList = [];
+
+var firstTime = true;
+
+function getFormInput(){
+    if(firstTime){
+        console.log("Running t18_validation.js");
+        firstTime = false;
+    }
+    if(ITEMFIELD.value != ""){
+        shoppingList.push(ITEMFIELD.value);
+        ITEMFIELD.value = "";
+    }
+
+    OUTPUT.innerHTML = "You have added " + shoppingList[shoppingList.length - 1] +" to the list";
+}
+function showList(){
+    OUTPUT.innerHTML = "You have added " + shoppingList[shoppingList.length - 1] +" to the list";
+    OUTPUT.innerHTML += "<ul>";
+    OUTPUT.innerHTML += "These are the items on your shopping list:";
+
+    for (let i = 0; i < shoppingList.length; i++) {
+        OUTPUT.innerHTML += `<li style="margin-left: 20px;">` + shoppingList[i] + "</li>";
+    }
+
+    OUTPUT.innerHTML += "</ul>";
+}
